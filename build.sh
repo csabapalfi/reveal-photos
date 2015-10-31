60f2a9f49ec52ba7bdb92ec937eb7719ec96bcdd
Csaba Palfi <csaba@palfi.me>
2015-10-31 09:12:05 +0100

    simplify script
    Signed-off-by: Csaba Palfi <csaba@palfi.me>

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # not nice but works
-# coded this on a plane, couldn't google better solutions
 function build {
-  SPLIT=$(grep -n PHOTOS < index.html.template | cut -d : -f 1)
-  awk "NR<$SPLIT {print}" < index.html.template
+  sed -n -e '/PHOTOS/,$!p' index.html.template
   for photo in $(find ./photos -name '*.jpg'); do
     echo "      <section data-background=\"$photo\"></section>"
   done
-  awk "NR>$SPLIT {print}" < index.html.template
+  sed -n -e '1,/PHOTOS/!p' index.html.template
 }
 
 build > index.html
